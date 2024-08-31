0278d04584eb98e0371db6247726203f303673b5
HasibGit <hasibullah04@gmail.com>
2024-08-31 14:00:21 +0600

    refactored court component

--- src/components/Court/Court.tsx
+++ src/components/Court/Court.tsx
@@ -9,7 +9,7 @@ import {
 } from "../../interfaces/app.interface";
 import { CourtService } from "../../services/courtService";
 
-const Court: React.FC<ICourtProps> = ({ rows, cols, player1, player2 }) => {
+const Court: React.FC<ICourtProps> = ({ rows, cols }) => {
   const [dotsToBeConnected, setDotsToBeConnected] = useState<IDotCoordinate[]>(
     []
   );
@@ -21,315 +21,8 @@ const Court: React.FC<ICourtProps> = ({ rows, cols, player1, player2 }) => {
 
   useEffect(() => {
     courtService.calculateLines(lines, setLines, rows, cols);
-    calculateBoxes();
+    courtService.calculateBoxes(boxes, setBoxes, rows, cols);
   }, []);
-
-  const calculateBoxes = () => {
-    const boxesCopy = [...boxes];
-
-    for (let i = 0; i < rows - 1; i++) {
-      for (let j = 0; j < cols - 1; j++) {
-        const firstLine: ILine = {
-          start: { i, j },
-          end: { i, j: j + 1 },
-          connected: false,
-        };
-
-        const secondLine: ILine = {
-          start: { i, j: j + 1 },
-          end: { i: i + 1, j: j + 1 },
-          connected: false,
-        };
-
-        const thirdLine: ILine = {
-          start: { i: i + 1, j: j + 1 },
-          end: { i: i + 1, j },
-          connected: false,
-        };
-
-        const forthLine: ILine = {
-          start: { i: i + 1, j },
-          end: { i, j },
-          connected: false,
-        };
-
-        boxesCopy.push({
-          firstLine,
-          secondLine,
-          thirdLine,
-          forthLine,
-          connected: false,
-          connectedBy: "",
-        });
-      }
-    }
-
-    setBoxes(boxesCopy);
-  };
-
-  const handleMouseHover = (i: number, j: number, direction: string) => {
-    if (direction == "horizonal") {
-      const firstDot: IDotCoordinate = { i, j };
-      const secondDot: IDotCoordinate = { i, j: j + 1 };
-
-      const newDotsToBeConnected = [firstDot, secondDot];
-      setDotsToBeConnected(newDotsToBeConnected);
-    }
-
-    if (direction == "vertical") {
-      const firstDot: IDotCoordinate = { i, j };
-      const secondDot: IDotCoordinate = { i: i + 1, j };
-
-      const newDotsToBeConnected = [firstDot, secondDot];
-      setDotsToBeConnected(newDotsToBeConnected);
-    }
-  };
-
-  const isDotToBeConnected = (i: number, j: number): boolean => {
-    return dotsToBeConnected.some((dot) => dot.i == i && dot.j == j);
-  };
-
-  const isDotPartOfConnectedBox = (i: number, j: number): boolean => {
-    const isMatchingCoordinate = (line: ILine) =>
-      (line.start.i === i && line.start.j === j) ||
-      (line.end.i === i && line.end.j === j);
-
-    return boxes.some(
-      (box) =>
-        box.connected &&
-        [box.firstLine, box.secondLine, box.thirdLine, box.forthLine].some(
-          isMatchingCoordinate
-        )
-    );
-  };
-
-  const isLinePartOfConnectedBox = (
-    i: number,
-    j: number,
-    alignment: string
-  ): boolean => {
-    if (alignment == "horizonal") {
-      const firstDot: IDotCoordinate = { i, j };
-      const secondDot: IDotCoordinate = { i, j: j + 1 };
-
-      return boxes.some(
-        (box) =>
-          box.connected &&
-          [box.firstLine, box.secondLine, box.thirdLine, box.forthLine].some(
-            (line) =>
-              (line.start.i == firstDot.i &&
-                line.start.j == firstDot.j &&
-                line.end.i == secondDot.i &&
-                line.end.j == secondDot.j) ||
-              (line.end.i == firstDot.i &&
-                line.end.j == firstDot.j &&
-                line.start.i == secondDot.i &&
-                line.start.j == secondDot.j)
-          )
-      );
-    }
-
-    if (alignment == "vertical") {
-      const firstDot: IDotCoordinate = { i, j };
-      const secondDot: IDotCoordinate = { i: i + 1, j };
-
-      return boxes.some(
-        (box) =>
-          box.connected &&
-          [box.firstLine, box.secondLine, box.thirdLine, box.forthLine].some(
-            (line) =>
-              (line.start.i == firstDot.i &&
-                line.start.j == firstDot.j &&
-                line.end.i == secondDot.i &&
-                line.end.j == secondDot.j) ||
-              (line.end.i == firstDot.i &&
-                line.end.j == firstDot.j &&
-                line.start.i == secondDot.i &&
-                line.start.j == secondDot.j)
-          )
-      );
-    }
-
-    return false;
-  };
-
-  const isSameDot = (
-    firstDot: IDotCoordinate,
-    secondDot: IDotCoordinate
-  ): boolean => {
-    return firstDot.i == secondDot.i && firstDot.j == secondDot.j;
-  };
-
-  const isDotPositionsMatch = (
-    line: ILine,
-    firstDot: IDotCoordinate,
-    secondDot: IDotCoordinate
-  ): boolean => {
-    return (
-      (isSameDot(line.start, firstDot) && isSameDot(line.end, secondDot)) ||
-      (isSameDot(line.start, secondDot) && isSameDot(line.end, firstDot))
-    );
-  };
-
-  const handleLineClick = (i: number, j: number, direction: string) => {
-    if (direction == "horizonal") {
-      const firstDot: IDotCoordinate = { i, j };
-      const secondDot: IDotCoordinate = { i, j: j + 1 };
-
-      const linesCopy = [...lines];
-
-      linesCopy.forEach((line) => {
-        if (!line.connected && isDotPositionsMatch(line, firstDot, secondDot)) {
-          line.connected = true;
-        }
-      });
-
-      setLines(linesCopy);
-
-      const boxesCopy = [...boxes];
-
-      boxesCopy.forEach((box) => {
-        if (!box.connected) {
-          if (
-            !box.firstLine.connected &&
-            isDotPositionsMatch(box.firstLine, firstDot, secondDot)
-          ) {
-            box.firstLine.connected = true;
-          }
-          if (
-            !box.secondLine.connected &&
-            isDotPositionsMatch(box.secondLine, firstDot, secondDot)
-          ) {
-            box.secondLine.connected = true;
-          }
-          if (
-            !box.thirdLine.connected &&
-            isDotPositionsMatch(box.thirdLine, firstDot, secondDot)
-          ) {
-            box.thirdLine.connected = true;
-          }
-          if (
-            !box.forthLine.connected &&
-            isDotPositionsMatch(box.forthLine, firstDot, secondDot)
-          ) {
-            box.forthLine.connected = true;
-          }
-
-          if (
-            box.firstLine.connected &&
-            box.secondLine.connected &&
-            box.thirdLine.connected &&
-            box.forthLine.connected
-          ) {
-            box.connected = true;
-            box.connectedBy = "Hasib";
-          }
-        }
-      });
-
-      setBoxes(boxesCopy);
-    }
-
-    if (direction == "vertical") {
-      const firstDot: IDotCoordinate = { i, j };
-      const secondDot: IDotCoordinate = { i: i + 1, j };
-
-      const linesCopy = [...lines];
-
-      linesCopy.forEach((line) => {
-        if (!line.connected && isDotPositionsMatch(line, firstDot, secondDot)) {
-          line.connected = true;
-        }
-      });
-
-      setLines(linesCopy);
-
-      const boxesCopy = [...boxes];
-
-      boxesCopy.forEach((box) => {
-        if (!box.connected) {
-          if (
-            !box.firstLine.connected &&
-            isDotPositionsMatch(box.firstLine, firstDot, secondDot)
-          ) {
-            box.firstLine.connected = true;
-          }
-          if (
-            !box.secondLine.connected &&
-            isDotPositionsMatch(box.secondLine, firstDot, secondDot)
-          ) {
-            box.secondLine.connected = true;
-          }
-          if (
-            !box.thirdLine.connected &&
-            isDotPositionsMatch(box.thirdLine, firstDot, secondDot)
-          ) {
-            box.thirdLine.connected = true;
-          }
-          if (
-            !box.forthLine.connected &&
-            isDotPositionsMatch(box.forthLine, firstDot, secondDot)
-          ) {
-            box.forthLine.connected = true;
-          }
-
-          if (
-            box.firstLine.connected &&
-            box.secondLine.connected &&
-            box.thirdLine.connected &&
-            box.forthLine.connected
-          ) {
-            box.connected = true;
-            box.connectedBy = "Hasib";
-          }
-        }
-      });
-
-      setBoxes(boxesCopy);
-    }
-
-    if (turn == "player1") {
-      setTurn("player2");
-    } else {
-      setTurn("player1");
-    }
-  };
-
-  const isLineConnected = (
-    i: number,
-    j: number,
-    direction: string
-  ): boolean => {
-    if (direction == "horizonal") {
-      const firstDot: IDotCoordinate = { i, j };
-      const secondDot: IDotCoordinate = { i, j: j + 1 };
-
-      const line = lines.find(
-        (line) =>
-          line.connected && isDotPositionsMatch(line, firstDot, secondDot)
-      );
-
-      if (line) {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      const firstDot: IDotCoordinate = { i, j };
-      const secondDot: IDotCoordinate = { i: i + 1, j };
-
-      const line = lines.find(
-        (line) =>
-          line.connected && isDotPositionsMatch(line, firstDot, secondDot)
-      );
-
-      if (line) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  };
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
@@ -347,25 +40,56 @@ const Court: React.FC<ICourtProps> = ({ rows, cols, player1, player2 }) => {
               <>
                 <Dot
                   key={i + "" + j}
-                  shouldBlink={isDotToBeConnected(i, j)}
-                  isPartOfConnectedBox={isDotPartOfConnectedBox(i, j)}
+                  shouldBlink={courtService.isDotToBeConnected(
+                    i,
+                    j,
+                    dotsToBeConnected
+                  )}
+                  isPartOfConnectedBox={courtService.isDotPartOfConnectedBox(
+                    i,
+                    j,
+                    boxes
+                  )}
                 ></Dot>
                 {j < cols - 1 && (
                   <Line
                     key={i + "line" + j}
                     horizontal={true}
-                    isConnected={isLineConnected(i, j, "horizonal")}
-                    isPartOfConnectedBox={isLinePartOfConnectedBox(
+                    isConnected={courtService.isLineConnected(
                       i,
                       j,
-                      "horizonal"
+                      "horizonal",
+                      lines
+                    )}
+                    isPartOfConnectedBox={courtService.isLinePartOfConnectedBox(
+                      i,
+                      j,
+                      "horizonal",
+                      boxes
                     )}
                     onMouseEnter={() =>
-                      !isLineConnected(i, j, "horizonal") &&
-                      handleMouseHover(i, j, "horizonal")
+                      !courtService.isLineConnected(i, j, "horizonal", lines) &&
+                      courtService.handleMouseHover(
+                        i,
+                        j,
+                        "horizonal",
+                        setDotsToBeConnected
+                      )
                     }
                     onMouseLeave={() => setDotsToBeConnected([])}
-                    onLineClick={() => handleLineClick(i, j, "horizonal")}
+                    onLineClick={() =>
+                      courtService.handleLineClick(
+                        i,
+                        j,
+                        "horizonal",
+                        lines,
+                        boxes,
+                        setLines,
+                        setBoxes,
+                        turn,
+                        setTurn
+                      )
+                    }
                   />
                 )}
               </>
@@ -385,18 +109,41 @@ const Court: React.FC<ICourtProps> = ({ rows, cols, player1, player2 }) => {
                 <Line
                   key={i + "" + j}
                   horizontal={false}
-                  isConnected={isLineConnected(i, j, "vertical")}
-                  isPartOfConnectedBox={isLinePartOfConnectedBox(
+                  isConnected={courtService.isLineConnected(
                     i,
                     j,
-                    "vertical"
+                    "vertical",
+                    lines
+                  )}
+                  isPartOfConnectedBox={courtService.isLinePartOfConnectedBox(
+                    i,
+                    j,
+                    "vertical",
+                    boxes
                   )}
                   onMouseEnter={() =>
-                    !isLineConnected(i, j, "vertical") &&
-                    handleMouseHover(i, j, "vertical")
+                    !courtService.isLineConnected(i, j, "vertical", lines) &&
+                    courtService.handleMouseHover(
+                      i,
+                      j,
+                      "vertical",
+                      setDotsToBeConnected
+                    )
                   }
                   onMouseLeave={() => setDotsToBeConnected([])}
-                  onLineClick={() => handleLineClick(i, j, "vertical")}
+                  onLineClick={() =>
+                    courtService.handleLineClick(
+                      i,
+                      j,
+                      "vertical",
+                      lines,
+                      boxes,
+                      setLines,
+                      setBoxes,
+                      turn,
+                      setTurn
+                    )
+                  }
                 />
               ))}
             </div>
--- src/services/courtService.ts
+++ src/services/courtService.ts
@@ -1,4 +1,23 @@
-import { ILine } from "../interfaces/app.interface";
+import React from "react";
+import { IBox, IDotCoordinate, ILine } from "../interfaces/app.interface";
+
+const isSameDot = (
+  firstDot: IDotCoordinate,
+  secondDot: IDotCoordinate
+): boolean => {
+  return firstDot.i == secondDot.i && firstDot.j == secondDot.j;
+};
+
+const isDotPositionsMatch = (
+  line: ILine,
+  firstDot: IDotCoordinate,
+  secondDot: IDotCoordinate
+): boolean => {
+  return (
+    (isSameDot(line.start, firstDot) && isSameDot(line.end, secondDot)) ||
+    (isSameDot(line.start, secondDot) && isSameDot(line.end, firstDot))
+  );
+};
 
 export class CourtService {
   calculateLines(
@@ -32,5 +51,320 @@ export class CourtService {
     }
 
     setLines(linesCopy);
+  }
+
+  calculateBoxes(
+    boxes: IBox[],
+    setBoxes: React.Dispatch<React.SetStateAction<IBox[]>>,
+    rows: number,
+    cols: number
+  ): void {
+    const boxesCopy = [...boxes];
+
+    for (let i = 0; i < rows - 1; i++) {
+      for (let j = 0; j < cols - 1; j++) {
+        const firstLine: ILine = {
+          start: { i, j },
+          end: { i, j: j + 1 },
+          connected: false,
+        };
+
+        const secondLine: ILine = {
+          start: { i, j: j + 1 },
+          end: { i: i + 1, j: j + 1 },
+          connected: false,
+        };
+
+        const thirdLine: ILine = {
+          start: { i: i + 1, j: j + 1 },
+          end: { i: i + 1, j },
+          connected: false,
+        };
+
+        const forthLine: ILine = {
+          start: { i: i + 1, j },
+          end: { i, j },
+          connected: false,
+        };
+
+        boxesCopy.push({
+          firstLine,
+          secondLine,
+          thirdLine,
+          forthLine,
+          connected: false,
+          connectedBy: "",
+        });
+      }
+    }
+
+    setBoxes(boxesCopy);
+  }
+
+  handleMouseHover(
+    i: number,
+    j: number,
+    direction: string,
+    setDotsToBeConnected: React.Dispatch<React.SetStateAction<IDotCoordinate[]>>
+  ): void {
+    if (direction == "horizonal") {
+      const firstDot: IDotCoordinate = { i, j };
+      const secondDot: IDotCoordinate = { i, j: j + 1 };
+
+      const newDotsToBeConnected = [firstDot, secondDot];
+      setDotsToBeConnected(newDotsToBeConnected);
+    }
+
+    if (direction == "vertical") {
+      const firstDot: IDotCoordinate = { i, j };
+      const secondDot: IDotCoordinate = { i: i + 1, j };
+
+      const newDotsToBeConnected = [firstDot, secondDot];
+      setDotsToBeConnected(newDotsToBeConnected);
+    }
+  }
+
+  isDotToBeConnected(
+    i: number,
+    j: number,
+    dotsToBeConnected: IDotCoordinate[]
+  ): boolean {
+    return dotsToBeConnected.some((dot) => dot.i == i && dot.j == j);
+  }
+
+  isDotPartOfConnectedBox(i: number, j: number, boxes: IBox[]): boolean {
+    const isMatchingCoordinate = (line: ILine) =>
+      (line.start.i === i && line.start.j === j) ||
+      (line.end.i === i && line.end.j === j);
+
+    return boxes.some(
+      (box) =>
+        box.connected &&
+        [box.firstLine, box.secondLine, box.thirdLine, box.forthLine].some(
+          isMatchingCoordinate
+        )
+    );
+  }
+
+  isLinePartOfConnectedBox(
+    i: number,
+    j: number,
+    alignment: string,
+    boxes: IBox[]
+  ): boolean {
+    if (alignment == "horizonal") {
+      const firstDot: IDotCoordinate = { i, j };
+      const secondDot: IDotCoordinate = { i, j: j + 1 };
+
+      return boxes.some(
+        (box) =>
+          box.connected &&
+          [box.firstLine, box.secondLine, box.thirdLine, box.forthLine].some(
+            (line) =>
+              (line.start.i == firstDot.i &&
+                line.start.j == firstDot.j &&
+                line.end.i == secondDot.i &&
+                line.end.j == secondDot.j) ||
+              (line.end.i == firstDot.i &&
+                line.end.j == firstDot.j &&
+                line.start.i == secondDot.i &&
+                line.start.j == secondDot.j)
+          )
+      );
+    }
+
+    if (alignment == "vertical") {
+      const firstDot: IDotCoordinate = { i, j };
+      const secondDot: IDotCoordinate = { i: i + 1, j };
+
+      return boxes.some(
+        (box) =>
+          box.connected &&
+          [box.firstLine, box.secondLine, box.thirdLine, box.forthLine].some(
+            (line) =>
+              (line.start.i == firstDot.i &&
+                line.start.j == firstDot.j &&
+                line.end.i == secondDot.i &&
+                line.end.j == secondDot.j) ||
+              (line.end.i == firstDot.i &&
+                line.end.j == firstDot.j &&
+                line.start.i == secondDot.i &&
+                line.start.j == secondDot.j)
+          )
+      );
+    }
+
+    return false;
+  }
+
+  handleLineClick(
+    i: number,
+    j: number,
+    direction: string,
+    lines: ILine[],
+    boxes: IBox[],
+    setLines: React.Dispatch<React.SetStateAction<ILine[]>>,
+    setBoxes: React.Dispatch<React.SetStateAction<IBox[]>>,
+    turn: string,
+    setTurn: React.Dispatch<React.SetStateAction<string>>
+  ) {
+    if (direction == "horizonal") {
+      const firstDot: IDotCoordinate = { i, j };
+      const secondDot: IDotCoordinate = { i, j: j + 1 };
+
+      const linesCopy = [...lines];
+
+      linesCopy.forEach((line) => {
+        if (!line.connected && isDotPositionsMatch(line, firstDot, secondDot)) {
+          line.connected = true;
+        }
+      });
+
+      setLines(linesCopy);
+
+      const boxesCopy = [...boxes];
+
+      boxesCopy.forEach((box) => {
+        if (!box.connected) {
+          if (
+            !box.firstLine.connected &&
+            isDotPositionsMatch(box.firstLine, firstDot, secondDot)
+          ) {
+            box.firstLine.connected = true;
+          }
+          if (
+            !box.secondLine.connected &&
+            isDotPositionsMatch(box.secondLine, firstDot, secondDot)
+          ) {
+            box.secondLine.connected = true;
+          }
+          if (
+            !box.thirdLine.connected &&
+            isDotPositionsMatch(box.thirdLine, firstDot, secondDot)
+          ) {
+            box.thirdLine.connected = true;
+          }
+          if (
+            !box.forthLine.connected &&
+            isDotPositionsMatch(box.forthLine, firstDot, secondDot)
+          ) {
+            box.forthLine.connected = true;
+          }
+
+          if (
+            box.firstLine.connected &&
+            box.secondLine.connected &&
+            box.thirdLine.connected &&
+            box.forthLine.connected
+          ) {
+            box.connected = true;
+            box.connectedBy = "Hasib";
+          }
+        }
+      });
+
+      setBoxes(boxesCopy);
+    }
+
+    if (direction == "vertical") {
+      const firstDot: IDotCoordinate = { i, j };
+      const secondDot: IDotCoordinate = { i: i + 1, j };
+
+      const linesCopy = [...lines];
+
+      linesCopy.forEach((line) => {
+        if (!line.connected && isDotPositionsMatch(line, firstDot, secondDot)) {
+          line.connected = true;
+        }
+      });
+
+      setLines(linesCopy);
+
+      const boxesCopy = [...boxes];
+
+      boxesCopy.forEach((box) => {
+        if (!box.connected) {
+          if (
+            !box.firstLine.connected &&
+            isDotPositionsMatch(box.firstLine, firstDot, secondDot)
+          ) {
+            box.firstLine.connected = true;
+          }
+          if (
+            !box.secondLine.connected &&
+            isDotPositionsMatch(box.secondLine, firstDot, secondDot)
+          ) {
+            box.secondLine.connected = true;
+          }
+          if (
+            !box.thirdLine.connected &&
+            isDotPositionsMatch(box.thirdLine, firstDot, secondDot)
+          ) {
+            box.thirdLine.connected = true;
+          }
+          if (
+            !box.forthLine.connected &&
+            isDotPositionsMatch(box.forthLine, firstDot, secondDot)
+          ) {
+            box.forthLine.connected = true;
+          }
+
+          if (
+            box.firstLine.connected &&
+            box.secondLine.connected &&
+            box.thirdLine.connected &&
+            box.forthLine.connected
+          ) {
+            box.connected = true;
+            box.connectedBy = "Hasib";
+          }
+        }
+      });
+
+      setBoxes(boxesCopy);
+    }
+
+    if (turn == "player1") {
+      setTurn("player2");
+    } else {
+      setTurn("player1");
+    }
+  }
+
+  isLineConnected(
+    i: number,
+    j: number,
+    direction: string,
+    lines: ILine[]
+  ): boolean {
+    if (direction == "horizonal") {
+      const firstDot: IDotCoordinate = { i, j };
+      const secondDot: IDotCoordinate = { i, j: j + 1 };
+
+      const line = lines.find(
+        (line) =>
+          line.connected && isDotPositionsMatch(line, firstDot, secondDot)
+      );
+
+      if (line) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      const firstDot: IDotCoordinate = { i, j };
+      const secondDot: IDotCoordinate = { i: i + 1, j };
+
+      const line = lines.find(
+        (line) =>
+          line.connected && isDotPositionsMatch(line, firstDot, secondDot)
+      );
+
+      if (line) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
 }
